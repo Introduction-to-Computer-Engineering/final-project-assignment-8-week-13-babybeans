79c98dfa00cb1eda77dabd844e8712dafbdc66fd
Jessica Gardner <48143153+babybeans@users.noreply.github.com>
2019-11-18 15:50:42 -0700

    Update i2cWrite.js

--- i2cWrite.js
+++ i2cWrite.js
@@ -1,3 +1,5 @@
+//code that allows the coder to write to the specified address.
+
 basic.forever(function () {//repeats continually 
 
     pins.i2cWriteNumber(0x1E, 1, NumberFormat.UInt8LE)// writes a 1 to address 0x1E.
