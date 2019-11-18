763b5a5d787d3e86f2586f166c28b1bbc88f9e05
Jessica Gardner <48143153+babybeans@users.noreply.github.com>
2019-11-18 15:49:36 -0700

    Update i2cRead.js

--- i2cRead.js
+++ i2cRead.js
@@ -1,3 +1,5 @@
+//code that allows for reading from the specified address.
+
 basic.forever(function () {//repeats continually
     let value = pins.i2cReadNumber(0x1E, NumberFormat.Int8LE, false)// puts the number read into the value variable.
     basic.showNumber(value)// shows value on 5x5 led matrix
