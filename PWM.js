b001a2aeb9e1abf3dba92211bad7e5a1c38dcf9b
Jessica Gardner <48143153+babybeans@users.noreply.github.com>
2019-11-18 15:48:38 -0700

    Create PWM.js

---
+++ PWM.js
@@ -0,0 +1,19 @@
+//PWM function that increases by 5% each time the loop, then decreases by 5% each time.
+
+basic.forever(function () {//runs continually
+    for (let i = 9; i < 171; i += 9) {//increases the pulse with in increments of 5%
+                                        
+
+        basic.pause(100)//pause for the user to be able to see what is happening
+
+
+        pins.servoWritePin(AnalogPin.P0, i)//increases the duty cycle as i increments
+    }
+    for (let i = 171; i > 9; i += -9) {//decreases the pulse with increments of 5% 
+
+        basic.pause(100)//pause for the user to see what is happening
+
+        pins.servoWritePin(AnalogPin.P0, i)//decreases the duty cycle as i decrements
+    }
+
+})
