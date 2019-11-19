849bf9ea54bdb912acd5d55d185d46fdbb5ab735
Jessica Gardner <48143153+babybeans@users.noreply.github.com>
2019-11-18 19:30:53 -0700

    Update accelerometer.js

--- accelerometer.js
+++ accelerometer.js
@@ -1,5 +1,5 @@
 
-//writes to an arbitrary address
+// this code writes to the accelerometer
 basic.forever(function () {//runs continuously
     pins.i2cWriteNumber(0x19, 2055, NumberFormat.Int8LE)// writes to 0x19
 }) 
