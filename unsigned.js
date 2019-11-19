bbed03740e8382de2aaa15bfccba9b47a42269f9
Jessica Gardner <48143153+babybeans@users.noreply.github.com>
2019-11-18 19:36:30 -0700

    Update unsigned.js

--- unsigned.js
+++ unsigned.js
@@ -1,5 +1,6 @@
 
-//writes to an arbitrary address
+//writes to an internal address
 basic.forever(function () {//runs continuously
-    pins.i2cWriteNumber(0x1E, 2055, NumberFormat.UInt8LE)// writes to 0x33
+    pins.i2cWriteNumber(0x1E, 2055, NumberFormat.UInt8LE)// writes to 0x1E
+    basic.pause(100); //pause to so the user can see what is going on
 }) 
