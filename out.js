610fff4b5c84a407b8290013ada7b9e958d027dd
Jessica Gardner <48143153+babybeans@users.noreply.github.com>
2019-11-20 15:30:55 -0700

    Delete out.js

--- out.js
+++
@@ -1,7 +0,0 @@
-//code that is configured to the OUT of the built-in function generator
-
-basic.forever(function(){ //runs in a continous loop
-    pins.digitalWritePin(DigitalPin.P1, 1);//writes one to digital pin 1
-    basic.pause(100);//pauses so the user can see what is happening
-    pins.digitalWritePin(DigitalPin.P1, 0);// writes zero to digital pin 1
-}) 
